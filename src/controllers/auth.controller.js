1bede472cf773f81ea8a94d1e0367b607e0f2f16
Yazan26 <yazandawud75@gmail.com>
2025-09-16 12:07:18 +0200

    Add bcrypt and related dependencies
    Added bcrypt, express-session, node-addon-api, node-gyp-build, random-bytes, and uid-safe modules to node_modules. These dependencies are required for authentication and session management features.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,5 @@
 const AuthController = {
 
-  validate: (req, res, next) => {
-   next();
-  },
 
   login: (req, res, next) => {
     let {email, password} = req.body;
@@ -14,19 +11,7 @@ const AuthController = {
         req.session.user = user;
         res.redirect('/users');
       });
-  },
-  logout: (req, res, next) => {
-    req.method === 'GET' 
-    ? res.redirect('/auth/login', { title: 'Logout' }) 
-    : next();
-  },
-
-  isLoggedIn(req, res, next) {
-    req.method === 'GET' 
-    ? res.render('isLoggedIn', { title: 'Check Login' }) 
-    : next();
-  },
-
+  }
 };
 
 module.exports = AuthController;
